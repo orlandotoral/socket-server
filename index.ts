d483261942225054db767828e621d17d4fc27675
orlandotoral@gmail.com <Trufina74!#>
2022-02-06 17:31:29 +0100

    First commit Rest-Server

---
+++ index.ts
@@ -0,0 +1,20 @@
+import { SERVER_PORT } from "./global/environment";
+import Server from "./classes/server";
+import router from "./routes/router";
+import bodyParser from "body-parser";
+import cors from 'cors';
+
+const server = new Server();
+
+//Bodyparser
+server.app.use(bodyParser.urlencoded( { extended: true }));
+server.app.use(bodyParser.json());
+
+//CORS
+server.app.use(cors ({origin:true , credentials:true }) );
+
+server.app.use('/', router);
+
+server.start( () => {
+    console.log(`Server running in port ${ SERVER_PORT } ...`);
+} )
---
+++ readme.md
@@ -0,0 +1,19 @@
+
+# Socket-server
+
+REbuild nodu modules
+`````
+npm install
+````
+
+Build dist
+`````
+tsc -w
+````
+
+Run server
+
+``````
+node dist/
+nodemon dist/
+````
